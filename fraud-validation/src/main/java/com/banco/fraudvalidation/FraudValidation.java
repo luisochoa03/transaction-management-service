1a287d52459dc53955d4c7eb28b2bb02856a08e9
Luis Ochoa <luis.ochoa.villasmil@bbva.com>
2024-06-08 15:04:07 -0500

    chore: ad d

---
+++ fraud-validation/src/main/java/com/banco/fraudvalidation/FraudValidation.java
@@ -0,0 +1,12 @@
+package com.banco.fraudvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudValidation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudValidation.class, args);
+    }
+}
